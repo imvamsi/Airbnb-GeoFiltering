f83308ab1637bd97def65c6bcda178a8ef2771a1
imvamsi <krrishvamsi7@gmail.com>
2021-03-09 18:37:53 -0500

    Added the auth page setup using styled firebase ui to work with the firebase auth service

--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,28 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FC, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+const firebaseAuthConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false
+        }
+    ],
+    signInSuccessfulUrl: '/'
+}
+
+const FirebaseAuth: FC = () => {
+    return (
+        <div className='mt-16'>
+            <StyledFirebaseAuth
+                uiConfig={firebaseAuthConfig}
+                firebaseAuth={firebase.auth()}
+            />
+        </div>
+    )
+}
+
+export default FirebaseAuth
