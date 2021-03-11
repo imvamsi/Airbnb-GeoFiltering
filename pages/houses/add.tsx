83091a72c12cc961f435be2e2b4ed68fc1128ddf
imvamsi <krrishvamsi7@gmail.com>
2021-03-10 19:14:51 -0500

    protect the add house route from server side

--- pages/houses/add.tsx
+++ pages/houses/add.tsx
@@ -1,8 +1,20 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
 // import Layout from "src/components/layout";
 // import HouseForm from "src/components/houseForm";
 
 export default function Add() {
   return <div>Add</div>;
+}
+
+export const getServerSideProps: GetServerSideProps = async({req, res}) => {
+  const uid = await loadIdToken(req as NextApiRequest)
+
+  if(!uid) {
+    res.setHeader('location', '/auth')
+    res.statusCode = 302
+    res.end()
+  }
+
+  return { props: {}}
 }
