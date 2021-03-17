83fcc80bea362dfdb7a2ebb6117fc2e65614f133
imvamsi <krrishvamsi7@gmail.com>
2021-03-17 17:14:28 -0400

    Added the house form Added image preview in the form

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
 // import {
@@ -20,13 +20,15 @@ interface FormData {
     latitude: number,
     longitude: number,
     bedrooms: string
+    image: FileList
 }
 
 interface FormProps {}
 
 export default function HouseForm({}: FormProps) {
 
-    const [submitting, setSubmitting] = useState(false)
+    const [submitting, setSubmitting] = useState<boolean>(false)
+    const [previewImage, setPreviewImage] = useState<string>()
     const {register, watch, errors, handleSubmit, setValue} = useForm<FormData>({defaultValues: {}})
     const address = watch('address')
 
@@ -37,7 +39,7 @@ export default function HouseForm({}: FormProps) {
     }, [register])
 
     async function handleCreate(data: FormData) {
-
+        console.log(data, 'formData')
     }
 
     function onSubmit(data: FormData) {
@@ -62,8 +64,80 @@ export default function HouseForm({}: FormProps) {
                 defaultValue=""
                 />
                 {errors.address && <p>{errors.address.message}</p>}
-                {address && <p>{address}</p>}
             </div>
+            
+            {address && 
+            <>
+            <div className='mt-4'>
+                <label 
+                    htmlFor='image'
+                    className='p-4 border-dashed border-4 block border-gray-600 cursor-pointer'>
+                  Click to add your image (16:9)
+                </label>
+                <input 
+                  id='image' 
+                  name='image' 
+                  type='file' 
+                  accept='image/*' 
+                  style={{display: 'none'}}
+                  ref={register({
+                      validate: (fileList: FileList) => {
+                          if(fileList.length === 1) return true
+                          else return "Please Upload an Image"
+                      }
+                  })}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    if(e?.target?.files?.[0]){
+                        const file = e.target.files[0]
+                        const reader = new FileReader()
+                        reader.onload = () => {
+                            setPreviewImage(reader.result as string)
+                        }
+                        reader.readAsDataURL(file)
+                    }
+                  }}
+                />
+                {previewImage && 
+                    <img 
+                      src={previewImage}
+                      className='mt-4 object-cover'
+                      style={{width: '576px',height: `${9/16 * 576}px`}}
+                    />
+                }
+                {errors.image && <p>{errors.image.message}</p>}
+            </div>
+            <div className='mt-4'>
+                <label
+                   htmlFor='bedrooms'
+                   className='block'
+                >
+                    Beds
+                </label>
+                <input
+                    id='bedrooms'
+                    name='bedrooms'
+                    type='number'
+                    className='p-2'
+                    ref={register({
+                        required: "please enter the no of bedrooms",
+                        max: {value: 10, message: 'Wooah! Too large'},
+                        min: {value: 1, message: 'Must have atleast one bedroom to list'}
+                    })}
+                />
+                {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
+            </div>
+            <div className='mt-4'>
+                <button className='bg-blue-500 over: bg-blue-700 font-bold py-2 px-4 rounded'
+                type='submit'
+                disabled={submitting}
+                >
+                  Save
+                </button>{" "}  
+                <Link href='/'>
+                    <a>Cancel</a>
+                </Link>
+            </div>
+            </>}
         </form>
     )
 }
