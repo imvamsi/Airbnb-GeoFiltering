7462cfcb17d1be090c434043d2125b33f0ef5e95
imvamsi <krrishvamsi7@gmail.com>
2021-03-30 13:12:03 -0400

    added basic create apollo client function

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,5 +1,15 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
-// import { HttpLink } from "@apollo/client/link/http";
-// import { useMemo } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { HttpLink } from "@apollo/client/link/http";
+import { useMemo } from "react";
 
-export {};
+function createApolloClient() {
+    return new ApolloClient({
+        link: new HttpLink({uri: '/api/graphql', credentials: 'same-origin'}),
+        cache: new InMemoryCache(),
+        defaultOptions: {
+            watchQuery: {
+                fetchPolicy: 'cache-and-network'
+            }
+        }
+    })
+}
