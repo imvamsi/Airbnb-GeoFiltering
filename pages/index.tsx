e723c2739f66521740f847b10499856bafbb50ec
imvamsi <krrishvamsi7@gmail.com>
2021-04-02 16:52:18 -0400

    added the front end graphql query to fetch the houses in the bounds and generated types for the queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
@@ -8,8 +8,33 @@ import Map from "src/components/map";
 import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
+const HOUSES_QUERY = gql`
+  query HousesQuery($bounds: BoundsInput!) {
+    houses(bounds: $bounds) {
+      id
+      latitude
+      longitude
+      address
+      publicId
+      bedrooms
+    }
+  }
+`;
 type Bounds = [[number, number], [number, number]];
 
+function parseBounds(bounds: string) {
+  const boundsResult = JSON.parse(bounds) as Bounds;
+  return {
+    sw: {
+      latitude: boundsResult[0][1],
+      longitude: boundsResult[0][0],
+    },
+    ne: {
+      latitude: boundsResult[1][1],
+      longitude: boundsResult[1][0],
+    },
+  };
+}
 export default function Home() {
   const [dataBounds, setDataBounds] = useLocalState<string>(
     "bounds",
@@ -17,6 +42,11 @@ export default function Home() {
   );
 
   const [debouncedDataBounds] = useDebounce(dataBounds, 200);
+  const { data, error } = useQuery(HOUSES_QUERY, {
+    variables: {
+      bounds: parseBounds(debouncedDataBounds),
+    },
+  });
 
   return (
     <Layout
