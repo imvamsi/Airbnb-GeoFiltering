76851b579be93f962071b5decf8f3b6a0cd2c24e
imvamsi <krrishvamsi7@gmail.com>
2021-04-03 20:47:07 -0400

    Display all the created houses on the map Added the house info in a popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
-// import HouseList from "src/components/houseList";
+//import HouseList from "src/components/houseList";
+import { useLastData } from "src/utils/useLastData";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
-import { useLastData } from "../src/utils/useLastData";
 
 const HOUSES_QUERY = gql`
   query HousesQuery($bounds: BoundsInput!) {
@@ -20,47 +20,43 @@ const HOUSES_QUERY = gql`
     }
   }
 `;
+
 type BoundsArray = [[number, number], [number, number]];
 
-function parseBounds(boundsString: string) {
-  // if (boundsString !== undefined) {
-  const bounds = JSON.parse(boundsString) as BoundsArray;
-  return {
-    sw: {
-      latitude: bounds[0][1],
-      longitude: bounds[0][0],
-    },
-    ne: {
-      latitude: bounds[1][1],
-      longitude: bounds[1][0],
-    },
-  };
-  // }
-}
+const parseBounds = (boundsString: string) => {
+  if (boundsString !== undefined || null) {
+    const bounds = JSON.parse(boundsString) as BoundsArray;
+    return {
+      sw: {
+        latitude: bounds[0][1],
+        longitude: bounds[0][0],
+      },
+      ne: {
+        latitude: bounds[1][1],
+        longitude: bounds[1][0],
+      },
+    };
+  }
+};
+
 export default function Home() {
+  const [highlightedId, setHighlightedId] = useState<string | null>(null);
   const [dataBounds, setDataBounds] = useLocalState<string>(
     "bounds",
-    "[[0, 0], [0,0]]"
+    "[[0,0],[0,0]]"
   );
-
   const [debouncedDataBounds] = useDebounce(dataBounds, 200);
-  console.log(
-    "🚀 ~ file: index.tsx ~ line 48 ~ Home ~ debouncedDataBounds",
-    debouncedDataBounds
-  );
   const { data, error } = useQuery<HousesQuery, HousesQueryVariables>(
     HOUSES_QUERY,
     {
       variables: {
-        bounds: debouncedDataBounds && parseBounds(debouncedDataBounds),
+        bounds: parseBounds(debouncedDataBounds),
       },
     }
   );
   const lastData = useLastData(data);
-  if (error) return <Layout main={<div>Error loading houses</div>} />;
 
-  console.log(data, "bounds data");
-  console.log(lastData, "house in the bounds");
+  if (error) return <Layout main={<div>Error loading houses</div>} />;
 
   return (
     <Layout
@@ -68,12 +64,15 @@ export default function Home() {
         <div className="flex">
           <div
             className="w-1/2 pb-4"
-            style={{ maxHeight: "calc(100vh - 64px", overflowY: "scroll" }}
+            style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}
           >
             HouseList
           </div>
           <div className="w-1/2">
-            <Map setDataBounds={setDataBounds} />
+            <Map
+              setDataBounds={setDataBounds}
+              houses={lastData ? lastData.houses : []}
+            />
           </div>
         </div>
       }
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,33 +1,25 @@
-import React, { useRef, useState, ChangeEvent } from "react";
+import { useRef, useState } from "react";
 import Link from "next/link";
 import { Image } from "cloudinary-react";
-import ReactMapGL, {
-  ExtraState,
-  Marker,
-  Popup,
-  ViewportProps,
-  ViewState,
-} from "react-map-gl";
+import ReactMapGL, { Marker, Popup, ViewState, ExtraState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
-// import { HousesQuery_houses } from "src/generated/HousesQuery";
-// import { SearchBox } from "./searchBox";
+import { HousesQuery_houses } from "src/generated/HousesQuery";
+import { SearchBox } from "./searchBox";
 
-interface MapProps {
+interface IProps {
   setDataBounds: (bounds: string) => void;
+  houses: HousesQuery_houses[];
 }
 
-export default function Map({ setDataBounds }: MapProps) {
+export default function Map({ setDataBounds, houses }: IProps) {
+  const [selected, setSelected] = useState<HousesQuery_houses | null>(null);
   const mapRef = useRef<ReactMapGL | null>(null);
-  const [viewPort, setViewPort] = useLocalState<ViewState>("viewport", {
-    latitude: 45.5086157,
-    longitude: -73.5903112,
-    zoom: 11,
+  const [viewport, setViewport] = useLocalState<ViewState>("viewport", {
+    latitude: 43,
+    longitude: -79,
+    zoom: 10,
   });
-
-  function handleViewPort(viewPort: ViewportProps): void {
-    setViewPort(viewPort);
-  }
 
   function handleBounds(): void {
     if (mapRef.current) {
@@ -46,18 +38,64 @@ export default function Map({ setDataBounds }: MapProps) {
   return (
     <div className="text-black relative">
       <ReactMapGL
-        {...viewPort}
+        {...viewport}
         width="100%"
         height="calc(100vh - 64px)"
         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-        onViewportChange={(viewPort) => handleViewPort(viewPort)}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
         ref={(instance) => (mapRef.current = instance)}
         minZoom={5}
         maxZoom={15}
-        mapStyle="mapbox://styles/imvamsi/ckm3y1161c2p717op6vfwld8k"
+        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
         onLoad={() => handleBounds()}
         onInteractionStateChange={(e) => handleMapStateChange(e)}
-      ></ReactMapGL>
+      >
+        {houses.map((house) => (
+          <Marker
+            key={house.id}
+            latitude={house.latitude}
+            longitude={house.longitude}
+            offsetLeft={-15}
+            offsetTop={-15}
+          >
+            <button
+              style={{ width: "30px", height: "30px", fontSize: "30px" }}
+              type="button"
+              onClick={() => setSelected(house)}
+            >
+              <img src={"/home-solid.svg"} alt="house" className="w-8" />
+            </button>
+          </Marker>
+        ))}
+
+        {selected && (
+          <Popup
+            latitude={selected.latitude}
+            longitude={selected.longitude}
+            onClose={() => setSelected(null)}
+            closeOnClick={false}
+          >
+            <div className="text-center">
+              <h3 className="px-4">{selected.address.substr(0, 30)}</h3>
+              <Image
+                className="mx-auto my-4"
+                cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+                publicId={selected.publicId}
+                secure
+                dpr="auto"
+                quality="auto"
+                width={200}
+                height={Math.floor((9 / 16) * 200)}
+                crop="fill"
+                gravity="auto"
+              />
+              <Link href={`/houses/${selected.id}`}>
+                <a>View House</a>
+              </Link>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
     </div>
   );
 }
