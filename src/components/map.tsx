031bad998b9df8d619bb7726fa00ebb618c0a137
imvamsi <krrishvamsi7@gmail.com>
2021-04-06 13:53:51 -0400

    Added the search box to the map in homepage

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -66,6 +66,25 @@ export default function Map({ setDataBounds, houses, highlightedId }: IProps) {
         onLoad={() => handleBounds()}
         onInteractionStateChange={(e) => handleMapStateChange(e)}
       >
+        <div className="absolute top-0 w-full z-10 p-4">
+          <SearchBox
+            defaultValue=""
+            onSelectAddress={(_address, latitude, longitude) => {
+              if (latitude && longitude) {
+                setViewport((old) => ({
+                  ...old,
+                  latitude,
+                  longitude,
+                  zoom: 12,
+                }));
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          />
+        </div>
         {houses.map((house) => (
           <Marker
             key={house.id}
